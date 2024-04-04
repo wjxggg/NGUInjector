21db28aef384966ac9a865ce344abe5db2605dfd
JConquistador <jonathanbyrd2@hotmail.com>
2024-04-04 14:01:51 -0400

    adding a difficulty changer to the titan tab

--- NGUInjector/Main.cs
+++ NGUInjector/Main.cs
@@ -47,7 +47,7 @@ namespace NGUInjector
         private float _timeLeft = 10.0f;
         internal static SettingsForm settingsForm;
         internal static WishManager WishManager;
-        internal const string Version = "3.8.4";
+        internal const string Version = "3.8.5";
         private static int _furthestZone;
 
         private static string _dir;
