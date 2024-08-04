9fa8faeee5a6d199de302e32a3d824491961ab06
rus9394 <rus9394@yandex.ru>
2024-08-04 14:52:40 +0300

    Label fix

--- NGUInjector/SettingsForm.cs
+++ NGUInjector/SettingsForm.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Reflection.Emit;
 using System.Windows.Forms;
 using NGUInjector.Managers;
 using static NGUInjector.Main;
